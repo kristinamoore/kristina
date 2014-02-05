45515997a2c9dd859ba6284834bee8e26bfc484d
kristina moore <kmoore@chicagotechacademy.org>
2014-02-05 12:00:00 -0600

    inital commit

---
+++ second_app_2/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SecondApp2::Application.config.secret_key_base = 'c7d8160716c3adf9e07b112ca76b00bd19184cfbf1e0f23ef4b7352c84bdb37d02196abffd7e310ef1c25128d91abf0dde7392c0158bbbb4a9c7416b02f4b53c'
